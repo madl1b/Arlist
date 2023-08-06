d8d38685730188e47ee5a2a72c0174a5cb15b1c9
madl1b <74344492+madl1b@users.noreply.github.com>
2023-08-06 14:36:14 +1000

    Add files via upload
    Samples Database added

---
+++ playlistMaker/samples.py
@@ -0,0 +1,38 @@
+import sqlite3 as sl 
+
+def buildSamplesDatabase():
+    conn = sl.connect('samples.db')
+
+    c = conn.cursor()
+
+    samples = """
+        CREATE TABLE samples (
+            id text not NULL primary key,
+            album text,
+            name text, 
+            instrumenality integer, 
+            acousticness integer, 
+            speechiness integer, 
+            danceability integer 
+            album REFERENCES albums(id)
+        )
+    """
+
+    # albums table included to accommodate more album related features in the future.
+    albums = """
+        CREATE TABLE albums (
+            id text not NULL primary key,
+            name text
+        )
+    """
+
+    c.execute(albums)
+    c.execute(samples)
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
